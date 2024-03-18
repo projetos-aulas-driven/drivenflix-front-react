d1f35350516bedcee60e50c54c8ebe3261fea35c
leticia-chijo <leticia.chijo@gmail.com>
2024-03-18 19:34:56 -0300

    Aula - Componentização

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
-function Lista() {
+function Topo() {
     return (
-        <ul>
-            <li>Senhor dos aneis: a sociedade do anel</li>
-            <li>Senhor dos aneis: as duas torres</li>
-            <li>Senhor dos aneis: o retorno do rei</li>
+        <div class="topo">
+            <h1>DrivenFlix</h1>
+
+            <div>
+                <button>Inserir Nome</button>
+                <p>Bem-vindo(a), FULANA!</p>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbDexwez6fwjsz-N3lcR3P5xcQH5enbLr9Og&usqp=CAU" />
+            </div>
+        </div>
+    )
+}
+
+function Menu() {
+    return (
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Favoritos</a></li>
+            <li><a href="#">Contato</a></li>
         </ul>
+    )
+}
+
+function Conteudo() {
+    return (
+        <ul class="conteudo">
+            <li>Senhor dos anéis: a sociedade do anel</li>
+            <li>Senhor dos anéis: as duas torres</li>
+            <li>Senhor dos anéis: o retorno do rei</li>
+        </ul>
+    )
+}
+
+function App() { 
+    return (
+        <div>
+            <Topo />
+            <div class="row">
+                <Menu />
+                <Conteudo />
+            </div>
+        </div>
     );
 }
 
-const listaReact = Lista();
-const elemento = document.querySelector(".root");
-ReactDOM.render(listaReact, elemento);
+const root = createRoot(document.querySelector(".root"));
+root.render(<App />);
