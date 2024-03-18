9d37df8ca29f94c702ef044a1aa19c9bf723a3e6
leticia-chijo <leticia.chijo@gmail.com>
2024-03-18 19:07:16 -0300

    Aula - Introdução a React

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-const nome = prompt("Qual é o seu nome?");
-alert(`Olá, ${nome}`);
+import ReactDOM from "react-dom";
+
+function Lista() {
+    return (
+        <ul>
+            <li>Senhor dos aneis: a sociedade do anel</li>
+            <li>Senhor dos aneis: as duas torres</li>
+            <li>Senhor dos aneis: o retorno do rei</li>
+        </ul>
+    );
+}
+
+const listaReact = Lista();
+const elemento = document.querySelector(".root");
+ReactDOM.render(listaReact, elemento);
