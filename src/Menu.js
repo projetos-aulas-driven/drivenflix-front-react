99bc9bd1c2932d77d156d8a0b16fe856544ccc4b
leticia-chijo <leticia.chijo@gmail.com>
2024-03-19 19:58:48 -0300

    Aula - Imprimir array no JSX com map

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,14 @@
 export default function Menu() {
+
+    const menu = [
+		{ texto: "Home", link: "./home.html", imagem: "https://www.seekpng.com/png/full/375-3752606_homepage-icon-house-logo-png-white.png" },
+		{ texto: "Favoritos", link: "./favoritos.html", imagem: "https://icon-library.com/images/white-heart-icon-png/white-heart-icon-png-11.jpg" },
+		{ texto: "Contatos", link: "./contatos.html", imagem: "https://i.im.ge/2022/09/15/1X4f74.envelope.png" }
+    ]
+
     return (
         <ul class="menu">
-            <MenuItem texto="Home" link="./home.html" imagem="https://www.seekpng.com/png/full/375-3752606_homepage-icon-house-logo-png-white.png" />
-			<MenuItem texto="Favoritos" link="./favoritos.html" imagem="https://icon-library.com/images/white-heart-icon-png/white-heart-icon-png-11.jpg" />
-			<MenuItem texto="Contatos" link="./contatos.html" imagem="https://i.im.ge/2022/09/15/1X4f74.envelope.png" />
+            {menu.map(m => <MenuItem texto={m.texto} imagem={m.imagem} link={m.link}/>)}
         </ul>
     )
 }
