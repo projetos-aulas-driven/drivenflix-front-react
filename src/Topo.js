df2ccc257f8957908f2c4a88b806b5f7910f2dcf
leticia-chijo <leticia.chijo@gmail.com>
2024-03-19 18:49:06 -0300

    Aula - Variáveis no JSX, ternário

--- src/Topo.js
+++ src/Topo.js
@@ -1,12 +1,15 @@
 export default function Topo() {
+    const nome = prompt("Qual é o seu nome?");
+    const imagemUsario = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbDexwez6fwjsz-N3lcR3P5xcQH5enbLr9Og&usqp=CAU";
+
     return (
         <div class="topo">
             <h1>DrivenFlix</h1>
 
             <div>
                 <button>Inserir Nome</button>
-                <p>Bem-vindo(a), FULANA!</p>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbDexwez6fwjsz-N3lcR3P5xcQH5enbLr9Og&usqp=CAU" />
+                <p>{!nome ? "Seja bem-vindo(a) ao site!" : `Seja bem vindo(a), ${nome}!`}</p>
+                <img src={imagemUsario} />
             </div>
         </div>
     )
