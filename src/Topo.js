4948d9ae5544f2d76cfd01690768700a75651724
leticia-chijo <leticia.chijo@gmail.com>
2024-03-20 16:00:06 -0300

    Aula - useState

--- src/Topo.js
+++ src/Topo.js
@@ -1,9 +1,19 @@
+import React from "react";
+
 export default function Topo() {
-    let nome = "";
-    const imagemUsario = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbDexwez6fwjsz-N3lcR3P5xcQH5enbLr9Og&usqp=CAU";
+    const imagemInicial = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbDexwez6fwjsz-N3lcR3P5xcQH5enbLr9Og&usqp=CAU";
+
+    const [nome, setNome] = React.useState("");
+    const [avatar, setAvatar] = React.useState(imagemInicial);
 
     function inserirNome() {
-        nome = prompt("Qual é o seu nome?");
+        const nomeDigitado = prompt("Qual é o seu nome?");
+        setNome(nomeDigitado);
+    }
+
+    function mudaImagem() {
+        const linkDigitado = prompt("Digite o link da sua imagem");
+        setAvatar(linkDigitado);
     }
 
     return (
@@ -13,7 +23,7 @@ export default function Topo() {
             <div>
                 <button onClick={inserirNome}>Inserir Nome</button>
                 <p>{!nome ? "Seja bem-vindo(a) ao site!" : `Seja bem vindo(a), ${nome}!`}</p>
-                <img onClick={() => alert("CLicou na imagem")} src={imagemUsario} />
+                <img onClick={mudaImagem} src={avatar} />
             </div>
         </div>
     )
